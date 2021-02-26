e2bf9f6ef563375a0a2982e093f38261e3bedca5
Vinicus <viniciusalcantarabarros@gmail.com>
2021-02-26 01:42:53 -0300

    Condição if

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const app = express()
 //Estou dizendo para o express usar o EJS como view enginne
 app.set('view engine', 'ejs')
 
-app.get("/", (req, res) => {
-    res.render("index")
+app.get("/:nome/:lang", (req, res) => {    
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = false
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        enterprise: "My house",
+        habilitys: "4",
+        msg: exibirMsg
+    })
 })
 
 app.listen(8001, ()=> {console.log("Hello world")})
